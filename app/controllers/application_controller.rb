32305df1510e51c088543abfc02f1b4394cf31a4
drame826 <dianguineds04@gmail.com>
2023-03-25 08:35:32 +0000

    correction

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+    include SessionsHelper
+
+    before_action :login_required
+   
+    private
+
+  def login_required
+    redirect_to new_session_path, alert: "ログインしてください" unless current_user
+  end
+
+end
